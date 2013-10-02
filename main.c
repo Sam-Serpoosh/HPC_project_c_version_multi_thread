26a5e5929d685e9c6b78605c742a6271ca5cafc3
Sam-Serpoosh <sam.serpoosh@gmail.com>
2013-10-02 16:32:31 -0400

    Get actual file content and run against real data.

--- main.c
+++ main.c
@@ -1,12 +1,29 @@
 #include "similarity_calculator.h"
+#include <stdio.h>
+
+char* read_file_content();
 
 int main(int argc, const char * argv[]) {
-	char* polymerase = "ATGGATC";
-	char* sequence = "ACGTCAG";
-	//char* polymerase = "GAC";
-	//char* sequence = "GTC";
+  char* sequence = read_file_content("/Users/Sam/CppPrograms/HPC_project_c_version/HIV-1_db.fasta.txt");
+  char* polymerase = read_file_content("/Users/Sam/CppPrograms/HPC_project_c_version/HIV_polymerase.fasta.txt");
+
   initialize(polymerase, sequence);
   calculate_similarity();
   pretty_print();
 	return 0;
+}
+
+char* read_file_content(char* file_path) {
+  long f_size;
+  char* content;
+  size_t code_s;
+  FILE* fp = fopen(file_path, "r");
+  fseek(fp, 0, SEEK_END);
+  f_size = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+  code_s = sizeof(char) * f_size;
+  content = malloc(code_s);
+  fread(content, 1, f_size, fp);
+
+  return content;
 }
