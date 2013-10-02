42ca280bf4a524b58842045b3e13fe721693cd59
Sam-Serpoosh <sam.serpoosh@gmail.com>
2013-10-01 20:38:20 -0400

    C version of Similarity Matrix Calculator with NO threads.

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "similarity_calculator.h"
+
+int main(int argc, const char * argv[]) {
+	//char* polymerase = "ATGGATC";
+	//char* sequence = "ACGTCAG";
+	char* polymerase = "GAC";
+	char* sequence = "GTC";
+  initialize(polymerase, sequence);
+  calculate_similarity();
+  pretty_print();
+	return 0;
+}
---
+++ similarity_calculator.h
@@ -0,0 +1,34 @@
+#include "similarity_matrix.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#ifndef similarity_matrix_similarity_calculator_h
+#define similarity_matrix_similarity_calculator_h
+
+void initialize(char* polymerase, char* sequence);
+void calculate_similarity();
+void pretty_print();
+
+// Instance Variables
+char* _polymerase;
+char* _sequence;
+long _column_numbers;
+struct SimilarityMatrix _sim_matrix;
+int** _similarity_matrix;
+long _row_numbers;
+//pthread_t* _threads;
+
+// Methods
+void initialize_similarity_matrix();
+void* calculate_neighbors_values_in_row(void* arg);
+void initialize_first_row();
+void initialize_first_column();
+int calculate_for_gap_in_polymerase(long row_index, long column_index);
+int calculate_for_gap_in_sequence(long row_index, long column_index);
+int calculate_for_match_or_mismatch(long row_index, long column_index);
+int max(int numbers[]);
+int length(int* arr);
+
+#endif
